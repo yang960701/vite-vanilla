632b045ae64f3a15495a889ba3fac2cb0c3bf2b1
yang <yang960701@gmail.com>
2025-04-07 11:03:46 +0900

    追加：バニラ勉強

---
+++ src/Study/Welcome.tsx
@@ -0,0 +1,28 @@
+import { Title, Text, Anchor, Button } from "@mantine/core";
+import * as classes from "./Welcome.css";
+
+export function Welcome() {
+  return (
+    <>
+      <Title className={classes.title} ta="center" mt={100}>
+        Welcome to{" "}
+        <Text
+          inherit
+          variant="gradient"
+          component="span"
+          gradient={{ from: "pink", to: "yellow" }}
+        >
+          Mantine
+        </Text>
+      </Title>
+      <Text color="dimmed" ta="center" size="lg" maw={580} mx="auto" mt="xl">
+        test
+        <Anchor href="https://mantine.dev/guides/vite/" size="lg">
+          this guide
+        </Anchor>
+        . To get started edit pages/Home.page.tsx file.
+      </Text>
+      <Button></Button>
+    </>
+  );
+}
